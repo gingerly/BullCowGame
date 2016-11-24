f55230e54d92c04a5908611b1248606548c13309
gingerly <haris.jeelani@gmail.com>
2016-11-23 20:27:31 -0500

    Checking if local is synced with remote

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -6,10 +6,41 @@
 //  Copyright © 2016 Haris Jeelani. All rights reserved.
 //
 
-#include <iostream>
+#include <iostream> // Pre-Processor directive
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// the entry point of our application
+int main(){
+    
+    PrintIntro();
+    
+    GetGuessAndPrintBack();
+    GetGuessAndPrintBack();
+
+    cout << endl;
     return 0;
+}
+
+void PrintIntro(){
+    // Intro the game.
+    constexpr int WORD_LENGTH = 8;
+    cout << "Welcome to the Bulls and Cows, a fun word game.\n";
+    cout << "Can you guess the "<< WORD_LENGTH;
+    cout << " letter isogram I'm thinking of?\n";
+    cout << endl;
+    return;
+}
+
+string GetGuessAndPrintBack(){
+    // Get a guess from the player.
+    string Guess = "";
+    cout <<"Enter your Guess: ";
+    getline(cin, Guess);
+    
+    cout << "You Guessed: " << Guess << endl;
+    return Guess;
 }
