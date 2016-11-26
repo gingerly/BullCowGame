9bba698e60c451736764d2ccb1049e5ca184f62d
gingerly <haris.jeelani@gmail.com>
2016-11-26 09:24:42 -0500

    Modified Read Me and cleaned the main code

---
+++ BullCowGame/main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  BullCowGame
+//
+//  Created by Haris Jeelani on 11/25/16.
+//  Copyright © 2016 Haris Jeelani. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+
+#endif /* main_h */
